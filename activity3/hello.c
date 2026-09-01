d852873a619f0c6b160432db7d38ed12077a00a7
PRAJWAL NAIK <chiefdraco101@gmail.com>
2026-09-01 11:29:49 +0530

    Added greet() fucntion -paired with Tanmayi via Live Share

---
+++ activity3/hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+void greet(const char* name)
+{
+    printf("Hello, %s! Welcome to my GitHub portfolio.\n", name);
+}
+int main()
+{
+    greet("Tanmayi Hii");
+    return 0;
+}
